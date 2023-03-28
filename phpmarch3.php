35cdc26604c9874274baef3d25fb69182630841d
Violeteyes24 <91657520+Violeteyes24@users.noreply.github.com>
2023-03-28 11:55:48 +0800

    Add files via upload

---
+++ phpmarch3.php
@@ -0,0 +1,15 @@
+<?php
+//$txt = "PHP";
+// $PHP datatype chuy ky flexible
+//echo "I love $txt!";
+// echo for printf
+// 1 <= 10 true  
+// for (initialize; condition; increment)
+$num = 5;
+$prod; 
+for($i = 0; $ i <= 10; $i++){ // loop 0 
+    $prod = $num*$i;
+    echo "$num x $i = $prod<br>";
+}
+
+?>
